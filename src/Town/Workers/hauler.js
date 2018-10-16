6fd3c74e314264cf128ccef84134f7426dbe702b
BuildTools <unconfigured@null.spigotmc.org>
2018-10-16 12:03:52 -0600

    Rejigged hauler logic to dump all energy before going for pickup

--- src/Town/Workers/hauler.js
+++ src/Town/Workers/hauler.js
@@ -4,14 +4,14 @@ const Hauler = {
         return [CARRY, CARRY, MOVE, MOVE];
     },
     run: function(creep, source, target) {
-        if(creep.carry.energy < creep.carryCapacity) {
-            if(creep.withdraw(source, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
-                creep.travelTo(source);
+        if(creep.carry.energy > 0) {
+            if(creep.transfer(target, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+                creep.travelTo(target);
             }
         }
         else {
-            if(creep.transfer(target, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
-                creep.travelTo(target);
+            if(creep.withdraw(source, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+                creep.travelTo(source);
             }
         }
     },
